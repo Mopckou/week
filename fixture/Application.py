0aee2c85e3307620f1fe0ec140becc2a326ef403
Mopckou <ermakov.a@list.ru>
2017-04-28 01:34:42 +0300

    регистр

--- fixture/Application.py
+++ fixture/Application.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
-from fixture.Session import SessionHelper
+from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
 class Application:
